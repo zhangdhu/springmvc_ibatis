9d4bf98ddca815a6b74292aa07da4c1c20ec7da1
zhangdhu <200715251@qq.com>
2015-08-07 08:42:18 +0800

    完成

--- src/main/java/com/zdh/service/UserInfoService.java
+++ src/main/java/com/zdh/service/UserInfoService.java
@@ -1,7 +1,10 @@
 package com.zdh.service;
 
+import com.zdh.model.UserInfo;
+
 /**
  * Created by zhangdh-a on 15-8-6.
  */
 public interface UserInfoService {
+    public Integer insert(UserInfo userInfo);
 }
